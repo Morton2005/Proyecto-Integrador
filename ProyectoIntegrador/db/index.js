5dfbc7838cbe5b57c894be10a8a85771b912b210
Santivivot <svivot@udesa.edu.ar>
2025-04-12 20:36:10 -0300

    db index.js

--- ProyectoIntegrador/db/index.js
+++ ProyectoIntegrador/db/index.js
@@ -12,14 +12,28 @@ const data = {
     productos:[
         {
             imagen:'/images/products/reloj1.webp',
-            nombre:'',
-            descripcion:'',
+            nombre:'Audemars-piguet',
+            descripcion:'Reloj de calidad, color gris, buen estado',
             id:1,
             comentarios:[
                 {
                     nombreDeUsuario:'Fernanditotorres96',
                     fotoDePerfil:'../public/images/users/default-image.png',
                     texto:'Muy bueno!',
+                }
+                
+            ],
+        },
+        {
+            imagen:'/images/products/relo2.jpeg',
+            nombre:'Biden',
+            descripcion:'Reloj de calidad, color azul y dorado, nuevo sin uso',
+            id:2,
+            comentarios:[
+                {
+                    nombreDeUsuario:'juanAlberto14',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'Impecable!',
                 }
                 
             ],
