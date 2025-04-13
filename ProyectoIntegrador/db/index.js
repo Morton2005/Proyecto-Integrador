4bceb8078257175d81aefacfc0b4512654eb137b
MiloVallacco <Kmilovallacco@gmail.com>
2025-04-13 18:06:11 -0300

    agregue los comentarios de todos los productas, termine tmb los productos. todo esto en el index en la carpeta de db

--- ProyectoIntegrador/db/index.js
+++ ProyectoIntegrador/db/index.js
@@ -17,10 +17,30 @@ const data = {
             id:1,
             comentarios:[
                 {
-                    nombreDeUsuario:'Fernanditotorres96',
+                    nombreDeUsuario:'JRB',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'Meh!',
+                },
+                {
+                    nombreDeUsuario:'UnBeso05',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'Maso maso!',
+                },
+                {
+                    nombreDeUsuario:'retro',
                     fotoDePerfil:'../public/images/users/default-image.png',
                     texto:'Muy bueno!',
-                }
+                },
+                {
+                    nombreDeUsuario:'AlfonsoAngeliono05',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'prefiero otras cosas!',
+                },
+                {
+                    nombreDeUsuario:'totin44',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'maso menos!',
+                },
                 
             ],
         },
@@ -34,7 +54,28 @@ const data = {
                     nombreDeUsuario:'juanAlberto14',
                     fotoDePerfil:'../public/images/users/default-image.png',
                     texto:'Impecable!',
-                }
+                },
+                {
+                    nombreDeUsuario:'tomi06',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'moooy bueno!',
+                },
+                {
+                    nombreDeUsuario:'ruben38',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me encanto!',
+                },
+                {
+                    nombreDeUsuario:'Fefo',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'muy raro!',
+                },
+                {
+                    nombreDeUsuario:'esteban',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'no me llama la atencion!',
+                },
+
                 
             ],
         },
@@ -47,8 +88,28 @@ const data = {
                 {
                     nombreDeUsuario:'camilalopez',
                     fotoDePerfil:'../public/images/users/default-image.png',
-                    texto:'Genial!',
-                }
+                    texto:'maso menos!',
+                },
+                {
+                    nombreDeUsuario:'Sofi10',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'lo compraria!',
+                },
+                {
+                    nombreDeUsuario:'Catu68',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'no me gusta mucho para ser sincera!',
+                },
+                {
+                    nombreDeUsuario:'toribio',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me gusta!',
+                },
+                {
+                    nombreDeUsuario:'agustina13',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'muy raro!',
+                },
                 
             ],
         },
@@ -59,11 +120,30 @@ const data = {
             id:4,
             comentarios:[
                 {
-                    nombreDeUsuario:'maria245',
+                    nombreDeUsuario:'cataa23',
                     fotoDePerfil:'../public/images/users/default-image.png',
                     texto:'Buenisimo!',
-                }
-                
+                },
+                {
+                    nombreDeUsuario:'asdc45',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'feaso!',
+                },
+                {
+                    nombreDeUsuario:'juani34',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'no vale la pena!',
+                },
+                {
+                    nombreDeUsuario:'memu12',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'sobrevalorado!',
+                },
+                {
+                    nombreDeUsuario:'santi10',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'muy buenooo!',
+                },
             ],
         },
         {
@@ -73,10 +153,202 @@ const data = {
             id:5,
             comentarios:[
                 {
+                    nombreDeUsuario:'LaPulga10',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'eh me guta mucho!',
+                },
+                {
                     nombreDeUsuario:'luis245',
                     fotoDePerfil:'../public/images/users/default-image.png',
                     texto:'Malisimo!',
-                }
+                },
+                {
+                    nombreDeUsuario:'Mila',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'muuuy buenoo<3!',
+                },
+                {
+                    nombreDeUsuario:'camilita12',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'meh!',
+                },
+                {
+                    nombreDeUsuario:'metso34',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'Malisimoooo',
+                },
+                
+            ],
+        },
+        {
+            imagen:'/images/products/reloj6.jpeg',
+            nombre:'Biden',
+            descripcion:'Reloj de calidad, color verde y plateado',
+            id:6,
+            comentarios:[
+                {
+                    nombreDeUsuario:'mMrton33',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me gusto mucho!',
+                },
+                {
+                    nombreDeUsuario:'DJ05',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me lo voy a llevar a una fiesta!',
+                },
+                {
+                    nombreDeUsuario:'Milo',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'Me quedaria muy bien',
+                },
+                {
+                    nombreDeUsuario:'cakso',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me parece muy sobrevaolorado',
+                },
+                {
+                    nombreDeUsuario:'pitu12',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'puaj!',
+                },
+                
+                
+            ],
+        },
+        {
+            imagen:'/images/products/relo7.jpeg',
+            nombre:'Luxury',
+            descripcion:'Reloj de calidad, color negro y plateado',
+            id:7,
+            comentarios:[
+                {
+                    nombreDeUsuario:'totosalvio',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'muy simple y comun!',
+                },
+                {
+                    nombreDeUsuario:'OgroFabiani',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'simplon!',
+                },
+                {
+                    nombreDeUsuario:'FrankFabra',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'mpara ir a comer coonn amigos esta bueno!',
+                },
+                {
+                    nombreDeUsuario:'Bauleti',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'tremendooo!',
+                },
+                {
+                    nombreDeUsuario:'PolloVigniolo',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'diferente al resto, me gusta',
+                },
+                
+            ],
+        },
+        {
+            imagen:'/images/products/relo8.jpeg',
+            nombre:'electronic',
+            descripcion:'Reloj de calidad, electrico',
+            id:8,
+            comentarios:[
+                {
+                    nombreDeUsuario:'Dj05',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'electrico, no me gusta! soy mas de lo antiguo',
+                },
+                {
+                    nombreDeUsuario:'Martin',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'dura mucho la bateria?',
+                },
+                {
+                    nombreDeUsuario:'Garrix',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'tiene pinta que se rompe facil',
+                },
+                {
+                    nombreDeUsuario:'Chapa',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me gusta todo la electronica, banco',
+                },
+                {
+                    nombreDeUsuario:'castelo',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'muy bueno para estar subiendo a la cabina con ese en la muñeca',
+                },
+                
+                
+            ],
+        },
+        {
+            imagen:'/images/products/relo9.jpeg',
+            nombre:'golden',
+            descripcion:'Reloj de calidad, color oro y verde',
+            id:9,
+            comentarios:[
+                {
+                    nombreDeUsuario:'juana  Gonzales',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'muy dorado no me gusta!',
+                },
+                {
+                    nombreDeUsuario:'fernandasoMalaso',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me llama la atencion la mezccla de colores',
+                },
+                {
+                    nombreDeUsuario:'trinidad',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'llama mucho la atencion, puede ser peligroso',
+                },
+                {
+                    nombreDeUsuario:'juana454',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me gusta como le queda el verde!',
+                },
+                {
+                    nombreDeUsuario: 'Gonzales',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'muy de banana ese',
+                },
+                
+            ],
+        },
+        {
+            imagen:'/images/products/relo10.jpeg',
+            nombre:'tomas',
+            descripcion:'Reloj de calidad, color marino y plateado',
+            id:10,
+            comentarios:[
+                {
+                    nombreDeUsuario:'fran lopez',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me  gusta, muy simple y lujoso parece!',
+                },
+                {
+                    nombreDeUsuario:'petson',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me  gusta, muy extrabagante',
+                },
+                {
+                    nombreDeUsuario:'sapo45',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'no me gusta',
+                },
+                {
+                    nombreDeUsuario:'cllapton12',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'me encanto',
+                },
+                {
+                    nombreDeUsuario:'fran lopez',
+                    fotoDePerfil:'../public/images/users/default-image.png',
+                    texto:'muuuy feo, no enteindo la gente que lo compra',
+                },
                 
             ],
         },
