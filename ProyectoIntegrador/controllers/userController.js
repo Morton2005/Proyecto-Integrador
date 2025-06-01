5185ad9f32d017ced96c04eaddd6bb14cdf1d154
Santivivot <svivot@udesa.edu.ar>
2025-06-01 16:50:41 -0300

    modificaciones en views, rutas y usercontroller

--- ProyectoIntegrador/controllers/userController.js
+++ ProyectoIntegrador/controllers/userController.js
@@ -3,42 +3,88 @@ let bcrypt = require('bcryptjs');
 
 const controller ={
 
+    register:function name (req,res){
+        if(req.session.user){
+            res.redirect('/users/profile'); 
+        }
+        return res.render('register')
+    },
+    
+
+    create: function(req, res){
+        if(req.body.email == ""){
+            return res.send("El email no puede estar vacío");
+        }
+
+        if(req.body.password.length < 3){
+            return res.send("La contraseña debe tener al menos 3 caracteres");
+        }
+
+        db.User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        .then(function(userFound){
+            if(userFound){
+                return res.send("El email ya existe")
+            }
+            return db.User.create({
+                username: req.body.username,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, 10),
+                birthdate: req.body.birthdate,
+                dni: req.body.dni,
+                profileImage: req.body.profileImage,
+                createdAt: new Date() 
+        })
+        })
+        .then(function(){
+            return res.redirect('/')
+        })
+        .catch(function(error) {
+            return res.send("Error al crear el usuario: " + error);
+        })
+    },
+
     show: function(req, res) {
         return res.render('register');    
     },
-    create: function(req, res){
-        db.User.create({
-             email: req.body.email,
-             password: bcrypt.hashSync(req.body.password, 10) 
-        })   
-        .then(function(User){
-             return res.redirect('/');
-        }) 
-         .catch(function (error){
-               return res.send(error);
-        });
-    },
+    
 
     profile:function name (req,res){
-            res.render('profile', {productos: data.productos,usuario:data.usuario});
+            res.render('profile');
     },
+
     login:function name (req,res){
+        if(req.session.user){
+            return res.redirect('/users/profile')
+        }
         res.render('login');
     },
 
-    register:function name (req,res){
-        res.render('register');
-    },
-    ProcesoDeLoggin: function(req, res) {
+    
+    loginProcess: function(req, res) {
         data.User.findOne({
             where: { email: req.body.email }
         })
-        .then(function(userFound) {
+        .then(function(usuario) {
+            if(!usuario){
+                return res.send("El email no está registrado.");
+            }
+
+            let contraseñaCorrecta = bcrypt.compareSync(req.body.password, usuario.password);
+
+            if(!contraseñaCorrecta){
+                return res.send("La contraseña es incorrecta.");
+            }
+
+            req.session.user = usuario;
+            return res.redirect('/users/profile');
            
-            res.send(userFound);
         })
         .catch(function(error) {
-            res.send(error);
+            return res.send("Error al procesar el login");
         });
     },
     
--- ProyectoIntegrador/routes/users.js
+++ ProyectoIntegrador/routes/users.js
@@ -8,5 +8,6 @@ const userController = require('../controllers/userController');
 router.get('/profile', userController.profile);
 router.get('/login', userController.login);
 router.get('/register', userController.register);
+router.post('/login',userController.loginProcess);
 
 module.exports = router;
