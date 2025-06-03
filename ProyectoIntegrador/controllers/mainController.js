a20ea95a4c0ce4c90e113484f00fd91e4bd29363
Morton2005 <dangelino@udesa.edu.ar>
2025-06-02 22:29:48 -0300

    agregue search results

--- ProyectoIntegrador/controllers/mainController.js
+++ ProyectoIntegrador/controllers/mainController.js
@@ -1,33 +1,47 @@
-const { where } = require('sequelize');
 let data = require('../database/models')
-let Op = db.sequelize.Op;
-const controller ={
-    index:function  (req,res){
-        data.Product.findAll()
-        .then(function(productos){
-        
-        return res.render('index', {productos:productos});
-        })
-      .catch(function(error){
-        return res.send("Error");
+let Op = data.Sequelize.Op;
+const controller = {
+  index: function (req, res) {
+    data.Product.findAll()
+      .then(function (productos) {
+        return res.render('index', { productos: productos });
       })
-    },
+      .catch(function (error) {
+        return res.send("Error");
+      });
+  },
 
-    searchResults: function(req, res) {
+  searchResults: function (req, res) {
     let search = req.query.search;
-    data.Producto.findAll({
-    where: [
-	{ title: { [Op.like]: `%${req.body.search}%` } }
-    ]
-  })
-   .then(function(productos) {
-      res.render("search-results"), {
-        title: "search",  
-        data: products,
-        search: search
-        
 
-}
-.catch ()
-}
-module.exports = controller;
+    data.Product.findAll({
+      where: {
+        nombre_producto: { [Op.like]: %${search}% }
+      },
+      include: [
+        { association: 'usuario' },
+        {
+          association: 'comentarios',
+          include: [{ association: 'usuario' }]
+        }
+      ]
+    })
+      .then(function (productos) {
+        if (productos.length > 0) {
+          return res.render("search-results", {
+            productos: productos,
+            search: search
+          });
+        } else {
+          return res.render("search-results", {
+            productos: [],
+            search: search,
+          });
+        }
+      })
+      .catch(function (error) {
+        return res.send("Error");
+      });
+  }
+};
+module.exports = controller;
--- ProyectoIntegrador/routes/index.js
+++ ProyectoIntegrador/routes/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 const productController = require('../controllers/productController');
+const mainController = require('../controllers/mainController');
 
+router.get('/search-results', mainController.searchResults);
 router.get('/', productController.home);
 
 module.exports = router;
